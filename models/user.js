bf588df0e09d26375526a6cf920dfc9ae89b0bcc
Danny Wheeler <wheeler@Dannys-MacBook-Pro.local>
2024-01-24 14:58:35 -0800

    user model

---
+++ models/user.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const jwt = require('jwt')
+
+const userSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    username: { type: String, required: true },
+    git_url: { type: String, required: false },
+    posts: [{type: mongoose.Schema.Types.ObjectId, ref: 'Post'}],
+    likes: [{type: mongoose.Schema.Types.ObjectId, ref: 'Post'}]
+}, { timestamps: true })
+
+// hash password
+userSchema.pre('save', async function(next) { 
+    if (this.isModified('password')) {
+        this.password = await bcrypt.hash(this.password, 8)
+    }
+    next()
+})
+
+// generate auth token method
+userSchema.methods.generateAuthToken = async function() {
+    const token = jwt.sign({ _id: this._id }, 'secret')
+    return token
+}
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User
